0453749e77364467304e31d4ed148a7ae17481cf
Anas Alnawati <anas.emad0717@gmail.com>
2026-07-24 20:50:58 +0300

    Create SmartClinicDB database with utf8mb4 charset

---
+++ SmartClinic.sql
@@ -0,0 +1,11 @@
+-- =========================================================
+-- 1. CREATE THE DATABASE
+-- =========================================================
+
+DROP DATABASE IF EXISTS SmartClinicDB;
+
+CREATE DATABASE SmartClinicDB
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
+
+USE SmartClinicDB;
